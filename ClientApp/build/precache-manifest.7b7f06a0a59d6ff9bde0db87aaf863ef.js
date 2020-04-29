bcfe85221c07e2d253ff99303839f8f11ff126f3
DevXendor <Xendor@live.cl>
2020-04-29 14:46:43 -0400

    publish files

---
+++ ClientApp/build/precache-manifest.7b7f06a0a59d6ff9bde0db87aaf863ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a844bc3a77b0ed95123952e22d27a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f910bf11de930ef8ff17",
+    "url": "/static/css/2.ecff023e.chunk.css"
+  },
+  {
+    "revision": "a3ebe06c99e1faf854bf",
+    "url": "/static/css/main.e016c294.chunk.css"
+  },
+  {
+    "revision": "f910bf11de930ef8ff17",
+    "url": "/static/js/2.607ca55d.chunk.js"
+  },
+  {
+    "revision": "9b194c4e46b44628daae438566d04ef5",
+    "url": "/static/js/2.607ca55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ebe06c99e1faf854bf",
+    "url": "/static/js/main.be956ad8.chunk.js"
+  },
+  {
+    "revision": "147b5da2ddd8fb1a36e4",
+    "url": "/static/js/runtime-main.58fd5853.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
